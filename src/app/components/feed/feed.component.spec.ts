d3abc079abc4a78edf9bd5addfe5d66153df2f52
maoindy <summao.com@gmail.com>
2021-01-25 17:24:35 +0700

    remove unit test

--- src/app/components/feed/feed.component.spec.ts
+++ src/app/components/feed/feed.component.spec.ts
@@ -4,22 +4,4 @@ import { FeedComponent } from './feed.component';
 
 describe('FeedComponent', () => {
   let component: FeedComponent;
-  let fixture: ComponentFixture<FeedComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FeedComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FeedComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
