cec7e2ebb65f1bc8e71c23f1e93b554ab9088c37
maoindy <summao.com@gmail.com>
2021-04-17 14:11:33 +0700

    add number of days and years

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -13,14 +13,30 @@ export class SignUpComponent implements OnInit {
   fg = new FormGroup({
     email: new FormControl(''),
     password: new FormControl(''),
+    profileName: new FormControl(''),
+    username: new FormControl(''),
+    month: new FormControl(''),
+    day: new FormControl(''),
+    year: new FormControl(''),
   });
+
+  days: number[] = [];
+  years: number[] = [];
 
   constructor(
     private accountService: AccountService,
     private router: Router
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    for (let i = 1; i <= 31; i++) {
+      this.days.push(i);
+    }
+
+    for (let i = 1990; i <= new Date().getFullYear(); i++) {
+      this.years.push(i);
+    }
+  }
 
   signup(): void {
     this.accountService.signup(this.fg.value).subscribe(
