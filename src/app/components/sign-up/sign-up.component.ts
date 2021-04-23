7dc22350ec51cacb4c321058255d6de46c037b14
maoindy <summao.com@gmail.com>
2021-04-23 23:41:16 +0700

    add feature sign up

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from 'src/app/services/authen/account.service';
 
@@ -10,14 +10,22 @@ import { AccountService } from 'src/app/services/authen/account.service';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  email = new FormControl('', [ Validators.required, Validators.maxLength(100), Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(20)]);
+  profileName = new FormControl('', [Validators.required, Validators.maxLength(100)]);
+  username = new FormControl('', [Validators.required, Validators.maxLength(30), Validators.pattern(/^[a-z][a-z_\d]*[a-z1-9]$/)]);
+  month = new FormControl('', [Validators.required]);
+  day = new FormControl('', [Validators.required]);
+  year = new FormControl('', [Validators.required]);
+
   fg = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-    profileName: new FormControl(''),
-    username: new FormControl(''),
-    month: new FormControl(''),
-    day: new FormControl(''),
-    year: new FormControl(''),
+    email: this.email,
+    password: this.password,
+    profileName: this.profileName,
+    username: this.username,
+    month: this.month,
+    day: this.day,
+    year: this.year,
   });
 
   days: number[] = [];
@@ -38,11 +46,11 @@ export class SignUpComponent implements OnInit {
     }
   }
 
-  signup(): void {
+  onSubmit(): void {
     this.accountService.signup(this.fg.value).subscribe(
       result => {
         console.log(result);
-        this.router.navigate(['news-feed']);
+        // this.router.navigate(['news-feed']);
       },
       error => console.log(error)
     );
